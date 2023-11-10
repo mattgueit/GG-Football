4c65dc0b2b5fefeefd284cc6c9c8795fd9adcfe9
Matt Gueit <mattgueit@gmail.com>
2023-11-10 16:31:42 +1100

    Rearrange files and start removing template stuff

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>GG Football</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
